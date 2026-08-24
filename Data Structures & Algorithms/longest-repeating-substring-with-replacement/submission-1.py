212fad61bf221c0abce8055f47c50c471e71908b
ayesha02025S <ayesha0@vt.edu>
2026-08-24 13:11:49 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {} # hashmap to count occurrences of each character
+        result = 0 # longest substring we can create with k replacement
+        left = 0
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+
+            while(right-left+1)  - max(count.values()) > k:
+                #length of window - count of the most freq characters 
+                count[s[left]] -= 1
+                left +=1 
+            result = max(result, right - left + 1)
+        return result
+
+        
+        
